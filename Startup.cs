ee6ab858017c8f6b0ae17d836613bcd49236524d
Natanael <natanaelsantosbr@gmail.com>
2021-09-22 01:03:09 -0300

    Data Driven

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.OpenApi.Models;
 
 namespace Shop
 {
+    /*
+    Uma classe de inicialização, toda vez que o app inicializar vai pra essa classe
+    */
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -15,6 +18,11 @@ namespace Shop
         }
 
         public IConfiguration Configuration { get; }
+
+        /*
+                Quais servicos do .net ou outros serviços a aplicação irar usar
+        */
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -27,6 +35,12 @@ namespace Shop
             });
         }
 
+        /*
+        Como ou quais opções desse serviço irá usar
+        IWebHostEnvironment = Esta em producao ou em dev
+        IApplicationBuilder = Esta na aplicação
+
+        */
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -37,16 +51,27 @@ namespace Shop
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop v1"));
             }
 
+            //Força a api para https
             app.UseHttpsRedirection();
 
+            //Usar o padrão de rota do mvc
             app.UseRouting();
+
 
             app.UseAuthorization();
 
+            //Mapeamento dos endpoint
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
+
+        /*
+        O que é API Data Driven ou Orientado a dados ?
+        Um espelho do banco de dados CRUD
+
+        DDD voltado pra regra de negocio
+        */
     }
 }
