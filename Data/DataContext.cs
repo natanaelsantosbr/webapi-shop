712950a1b8e1ae61c808f566541460fb824f0957
Natanael <natanaelsantosbr@gmail.com>
2021-09-23 16:58:27 -0300

    DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,26 @@
+/*
+Fazer o mapeamento em relação ao banco de dados
+*/
+
+
+using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
