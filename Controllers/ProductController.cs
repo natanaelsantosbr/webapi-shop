fce0a9d6fd498792c576522c350d9e10386d91eb
Natanael <natanaelsantosbr@gmail.com>
2021-09-26 16:08:57 -0300

    Iniciando Controller de Produto

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<Product>> Get([FromServices] DataContext context)
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(products);
+        }
+
+    }
+}
