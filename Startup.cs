7bcc92595181e8be3110d772009b25fce92b46ec
Natanael <natanaelsantosbr@gmail.com>
2021-09-30 00:17:36 -0300

    Nao exibir a tela de erro

--- Startup.cs
+++ Startup.cs
@@ -96,10 +96,10 @@ namespace Shop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             //Força a api para https
             app.UseHttpsRedirection();
